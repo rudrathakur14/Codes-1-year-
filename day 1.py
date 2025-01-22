6067ee5e48295a079dc44c9600f6c384a2df588b
rudrathakur14 <anveshsingh0612@gmail.com>
2025-01-22 12:00:12 +0530

    Add files via upload

---
+++ day 1.py
@@ -0,0 +1,278 @@
+# import math
+# a=int(input("enter first side"))
+# b=int(input("enter the second side"))
+# c=int(input("enter the third side"))
+# s=a + b + c /2
+# print(s)
+# # s1=(s*((s-a)*(s-b)*(s-c)))**(1/2)
+# s1=(s*(s-a)*(s-b)*(s-c))
+# print(s1)
+# from operator import truediv
+#
+# from enum import nonmember
+
+# a=int(input("enter a number:"))
+# b=int(input("enter the another number:"))
+# print("before swapping value of a and b:",a,b)
+# temp = a
+# a = b
+# b = temp
+# print("after swapping value of a and b :",a,b)
+# a= a+b
+# b=a-b
+# a=a-b
+# print("after swapping value of a and b:",a,b)
+# f=float(input("enter a temp in fahrhenit:"))
+# fahrhenit=(celsius*1.8)+32
+# print("the temp in fahrenheit is:",fhahrenheit)
+# print("the temprature in celsius is:",celsius)
+
+
+# principal=float(input("enter the principal:"))
+# rate=float(input("enter the rate:"))
+# time=float(input("enter the time:"))
+# sI=(principal * rate * time)/100
+# print("simple intrest",sI)
+# import math
+# radius=float(input("enter the radius:"))
+# height=float(input("enter the height:"))
+# surface_area=2*math.pi*radius*(radius+height)
+# print("surface area of cylinder",surface_area)
+#
+# principal=float(input("enter the principal"))
+# rate=float(input("enter the rate:"))
+# time=float(input("enter the time:"))
+# # compound_intrest=principal*((1+(rate/100))
+#
+
+# a=int(input("enter num1"))
+# b=int(input("enter num2"))
+# c=int(input("enter num3"))
+# avg=(a+b+c)/3
+# print("the avrage of 3 num are",avg)
+
+# sub1=int(input("enter the subject marks"))
+# sub2=int(input("enter the subject marks"))
+# sub3=int(input("enter the subject marks"))
+# sub3=int(input("enter the subject name"))
+# sub4=int(input("enter the subject name"))
+# sub5=int(input("enter the subject name"))
+# percentage=((sub1+sub2+sub3+sub4+sub5)/500)*100
+# print("percentage of 5 subject are:",percentage,"%")
+# import math
+# a=int(input("enter raidus"))
+# perimeter =2*math.pi*r
+# print("perimeter of circle is:",perimeter)
+#
+# a=int(input("enter base"))
+# b=int(input("enter height"))
+# area=1/2*b*a
+# print("area of scalene of trapezium is: ",area)
+
+
+
+############ DAY 2 ##############
+# X=10
+# NAME="ALICE"
+# PI=4.13
+
+# x=20
+# y=10
+# z=3+7j
+# x1=10.5
+# print("the number are:",x,y,z,x1)
+#
+# my_list=[1,2,3]
+# my_tuple=(1,2,3)
+# my_name=range(5)
+# print("result:",my_list,my_tuple,my_name)
+
+# for i in range(100,0,-20):
+#     print(i)
+
+
+# my_dict={"name":"Alice","Age":"18"}
+# print(my_dict)
+# print(my_dict["name"])
+# print(my_dict["Age"])
+
+# my_set={1,2,3}
+# print(my_set)
+# my_frozenset=frozenset({1,2,3})
+# print(my_frozenset)
+# # none type
+# nothing = none
+# print(nothing)
+
+# x="10"
+# y=7
+# z=int(x)+y
+# print(z)
+
+
+# a=int(input("enter a number"))
+# b=int(input("enter the another number:"))
+# sum=a+b
+# print(sum)
+# sub=a-b
+# print(sub)
+# mul=a*b
+# print(mul)
+# div=a/b
+# print(div)
+# mod=a%b
+# print(mod)
+# floordiv=(a//b)
+# print(floordiv)
+
+
+#a=int(input("enter the numberr:"))
+#b=int(input("enter the number:"))
+# print(a==b)
+# print(a!=b)
+# print(a>b)
+# print(a<b)
+# print(a>=b)
+# print(a<=b)
+
+
+# a=True
+# b=False
+# print(a and b)
+# print(a or b)
+# print( not a and not b)
+
+# a=int(input("enter a number:"))
+# b=int(input("enter another number:"))
+# print(a&b)
+# print(a|b)
+# print(a^b)
+# print(~a)
+# print(a<<b)
+# print(a>>b)
+
+
+#Assignment operators
+ # ba=7
+ # print(a)
+ # a=a+7
+ # print(a)
+ # a+=7
+ # print(a)
+ # a-=7
+ # print(a)
+ # a*=7
+ # print(a)
+ # a/=7
+ # print(a)
+ # a%=7
+ # print(a)
+ # a//=7
+ # print(a)
+ # a**=7
+ # b=7
+ # b**=2
+ # print(b)
+
+
+# x=[1,2,3]
+# y=[1,2,3]
+# z=x
+# print(x is y)
+# print(x is z)
+# print(x is not y)
+
+# a=
+# print("result",a)
+
+# result=5+9
+# print(result)
+# # a=6
+# # B=6
+# print(a+b)
+# a=int(input())
+# b=int(input())
+# print(a+b)
+# print(a and b)
+# print(a or b)
+# print(a<b)
+# print(a>b)
+# print(a&b)
+# print(a|b)
+# print("hello wolrd"+"pyhton is a programing language")
+# str1="hello world"
+# str2="python is a programming language"
+# str3=str1+str2
+# print(str3)
+
+# s1='hello'
+# print(s1)
+# s2="alice"
+# print(s2)
+
+
+# s3='''this is
+# a multiline string'''
+# print(s3)
+# print(s3[11])
+# print(s1[0])
+# print(s2[0])
+# print(s3[0])
+# print(s1[4])
+# print(s2[4])
+# print(s3[4])
+
+
+# s4="hello this is a string"
+# print(s4)
+# print(s4[0:6])
+# print(s4[:6])
+# print(s4[7:])
+# print(s4[0:11:2])
+# print(s4[::2])
+# print(s4[::- 1])
+# s5="python is awesome. python is easy to use."
+# print(s5[0:12:3])
+# print((s5[0:4]))
+# print(s5[::2])
+# print(s5[6:])
+# print(s5[:6])
+# print(s5[::-3])
+
+# name="Thakur"
+# age=17
+# # print("my name is {} and my profession is {}.".format(name,profession))
+#
+# print("my name is %s and i am %d years ol-*d."%(name,age))
+s1="hello"
+s2="world"
+print(s1+s2)
+s3=s1+s2
+print(s3)
+print(s1*4)
+test ="hello world! this is a test string"
+print("python" in test)
+print("java" in test)
+print("hello" in test)
+print("python" not in test)
+print("java not in test")
+print("java not in test")
+
+print(test.count("Hello"))
+print(test.index("Hello"))
+print(test.index("World"))
+print(test.lower())
+print(test.upper())
+words=test.split()
+print(words)
+joined= " ".join(words)
+print(joined)
+print(test.replace("Hello", "Hello World"))
+print(test.find("Hello"))
+print(test.find("World"))
+print(test.startswith("Hello"))
+print(test.endswith("World"))
+
+
+
+
